39a3856e3712696cc04f5b501216cd1a1c867e68
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2021-04-18 21:35:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+gsap.to(".tweenblock", {
+    duration: 1, 
+    x: 100, 
+    y: 100, 
+    backgroundColor: '#32a852', 
+    borderRadius: 50});
+
+gsap.to(".tweenblock2", {
+    duration: 1, 
+    delay: 1,
+    x: 100, 
+    y: 100, 
+    backgroundColor: '#32a852', 
+    borderRadius: 50});

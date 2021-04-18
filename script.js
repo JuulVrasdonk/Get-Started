9d9b423651d104baace589799f91bba8c9ea66db
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2021-04-18 22:13:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ gsap.to(".tweenblock2", {
     rotation: 180,
     backgroundColor: '#3269a8', 
     borderRadius: '50% 5%'});
+
+gsap.to(".cssplugintext", {
+    duration: 1, 
+    opacity: .5,
+    repeat: -1,
+    yoyo: true
+});
+
+gsap.to('.transformblock', {
+    duration: 1,
+    rotationX: 360,
+    rotationY: 180,
+    borderRadius: 50,
+    backgroundColor: '#32a852', 
+});
